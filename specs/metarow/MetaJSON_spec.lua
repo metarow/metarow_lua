0f0d91c27365d508958e66a1d86f7d4133cd53d7
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2014-10-15 10:25:54 +0200

    added Stack class for RPN calculator

--- specs/metarow/MetaJSON_spec.lua
+++ specs/metarow/MetaJSON_spec.lua
@@ -12,15 +12,15 @@ describe( "basic functions", function ( )
     local d = MetaJSON()
     assert.is_not_nil( d )
     assert.is_true( type( d.data ) == 'table')
-    assert.are.equal( 0, #d.data )
+    assert.are.equals( 0, #d.data )
   end)
 
   it( "stores parsed JSON data", function( )
     local d = MetaJSON{ json = jsonString }
     assert.is_not_nil( d )
     assert.is_true( type( d.data ) == 'table')
-    assert.are.equal( 4, #d.data )
-    assert.are.equal( 'createRect', d.data[1].fun.name )
+    assert.are.equals( 4, #d.data )
+    assert.are.equals( 'createRect', d.data[1].fun.name )
   end)
 
   it( "sets data from new JSON string", function( )
@@ -35,12 +35,12 @@ describe( "basic functions", function ( )
       return 1
     end
     assert.is_true( type( d['createRect'] ) == 'function' )
-    assert.are.equal( 1, d[d.data[1].fun.name]( ) )
+    assert.are.equals( 1, d[d.data[1].fun.name]( ) )
   end)
 
   it( "handles MetaJSON key val", function( )
     local d = MetaJSON{ json = '{ "val" : 1 }' }
-    assert.are.equal( 1, d:getMeta( d.data ))
+    assert.are.equals( 1, d:getMeta( d.data ))
   end)
 
   it( "parses params key", function( )
@@ -64,8 +64,8 @@ describe( "basic functions", function ( )
       return 1024
     end
     local params = d:getParams( d.data )
-    assert.are.equal( 2, params.strokeWidth )
-    assert.are.equal( 768, params.width)
+    assert.are.equals( 2, params.strokeWidth )
+    assert.are.equals( 768, params.width)
   end)
 
   it( "handles MetaJSON key fun", function( )
@@ -79,6 +79,21 @@ describe( "basic functions", function ( )
     d.screenHeight = function( )
       return 1024
     end
-    assert.are.equal( 768, d:getMeta( d.data[1] ) )
+    assert.are.equals( 768, d:getMeta( d.data[1] ) )
+  end)
+end)
+
+describe( "calculation functions", function( )
+  it( "detects function calls", function( )
+    local d = MetaJSON
+    assert.is_true( d.isCall( "add()" ) )
+  end)
+  it( "handles MetaJSON key calc", function( )
+    local d = MetaJSON{ json = [[
+      {
+        "calc" : [10, 20, "add()"]
+      }
+    ]] }
+    assert.are.equals( 30, d:getMeta( d.data ))
   end)
 end)
