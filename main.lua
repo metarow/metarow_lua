f52fb126667567837864b0b56981fdae39d21ead
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2014-10-17 10:27:43 +0200

    read hello world from solution

--- main.lua
+++ main.lua
@@ -1,56 +1,8 @@
 local MetaMan = require"lib.metarow.MetaMan"
 
 local solutionName = "inventory"
-local root = MetaMan( solutionName )
+local root = MetaMan{ solutionName=solutionName }
 
-local jsonString = [[
-[
-  {
-    "fun" : {
-      "name" : "createRect",
-      "params" : {
-        "x" : { "val" : 0 },
-        "y" : { "val" : 0 },
-        "anchorX" : { "val" : 0 },
-        "anchorY" : { "val" : 0 },
-        "width" :  { "fun" : { "name":"screenWidth" }},
-        "height" : { "fun" : { "name":"screenHeight" }},
-        "fillColor" : { "val" : [0.6] }
-      }
-    }
-  },
-  {
-    "fun" : {
-      "name" : "createRect",
-      "params" : {
-        "x" : { "calc" : [ "screenWidth()", 2, "div()" ] },
-        "y" : { "calc" : [ "screenHeight()", 2, "div()" ] },
-        "anchorX" : { "val" : 0.5 },
-        "anchorY" : { "val" : 0.5 },
-        "width" :  { "val" : 250 },
-        "height" : { "val" : 70 },
-        "fillColor" : { "val" : [0.7, 0.5, 0.3] }
-      }
-    }
-  },
-  {
-    "fun" : {
-      "name" : "createText",
-      "params" : {
-        "text" : { "val" : "Hello world!" },
-        "x" : { "calc" : [ "screenWidth()", 2, "div()" ] },
-        "y" : { "calc" : [ "screenHeight()", 2, "div()" ] },
-        "anchorX" : { "val" : 0.5 },
-        "anchorY" : { "val" : 0.5 },
-        "fillColor" : { "val" : [0.9] },
-        "fontSize" : { "val" : 32 }
-      }
-    }
-  }
-]
-]]
-
-root.view:setData( jsonString )
+root.view:setData( root:getDefinition( 'view', 'index' ) )
 
 local group, templateName = root.view:exec( )
-
